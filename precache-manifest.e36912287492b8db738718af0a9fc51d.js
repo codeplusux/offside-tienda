ac1b7e6379aadecd9ddd9b245c0f8f24b02423d5
codeplusux <dev@pablovivanco.com>
2020-09-15 20:18:31 -0300

    Updates

---
+++ precache-manifest.e36912287492b8db738718af0a9fc51d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53ecb8b1e6c2288c9fee6cf29d51ac9",
+    "url": "/offside-tienda/index.html"
+  },
+  {
+    "revision": "ec18dd498dd7ccc2eb32",
+    "url": "/offside-tienda/static/js/2.0e7214de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/offside-tienda/static/js/2.0e7214de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f5d0af4afe37e7ea4a",
+    "url": "/offside-tienda/static/js/main.19d2184c.chunk.js"
+  },
+  {
+    "revision": "71aecde404fcadd86714",
+    "url": "/offside-tienda/static/js/runtime-main.1e4c9042.js"
+  }
+]);
