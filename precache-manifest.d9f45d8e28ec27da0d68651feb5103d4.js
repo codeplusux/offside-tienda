537b705c64c1306451ecf4b346b481e2810d84d1
codeplusux <dev@pablovivanco.com>
2020-09-17 11:42:12 -0300

    Updates

---
+++ precache-manifest.d9f45d8e28ec27da0d68651feb5103d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba292c00690f6ed3ce1fb13b045d60d",
+    "url": "/offside-tienda/index.html"
+  },
+  {
+    "revision": "3264fb2f7187d1807cb6",
+    "url": "/offside-tienda/static/css/main.242ac1e5.chunk.css"
+  },
+  {
+    "revision": "ec18dd498dd7ccc2eb32",
+    "url": "/offside-tienda/static/js/2.0e7214de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/offside-tienda/static/js/2.0e7214de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3264fb2f7187d1807cb6",
+    "url": "/offside-tienda/static/js/main.0016fab8.chunk.js"
+  },
+  {
+    "revision": "71aecde404fcadd86714",
+    "url": "/offside-tienda/static/js/runtime-main.1e4c9042.js"
+  },
+  {
+    "revision": "722c481c64873ee86f7ba64a4c3b580d",
+    "url": "/offside-tienda/static/media/sportBg.722c481c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/offside-tienda/precache-manifest.73369aa76970697d7a1063504831c1d5.js"
+  "/offside-tienda/precache-manifest.d9f45d8e28ec27da0d68651feb5103d4.js"
 );
 
 self.addEventListener('message', (event) => {
